9ba73ba7d30ecde50f2ef5a13a73ef38b2f31541
bwanglzu <bo.wang@jina.ai>
2021-06-24 11:46:43 +0200

    feat: mock integration test callback

--- tests/integration/test_text_paddle.py
+++ tests/integration/test_text_paddle.py
@@ -19,16 +19,27 @@ def document_array(content):
     return DocumentArray([Document(content=content)])
 
 
-@pytest.fixture(scope='function')
-def parameters(content):
-    return {'traverse_paths': ['r'], 'batch_size': 10}
+def validate_callback(mock, validate_func):
+    for args, kwargs in mock.call_args_list:
+        validate_func(*args, **kwargs)
+    mock.assert_called()
 
 
-def test_text_paddle(flow, content, document_array, parameters):
+@pytest.mark.parametrize(
+    'parameters',
+    [
+        {'traverse_paths': ['r'], 'batch_size': 10},
+        {'traverse_paths': ['m'], 'batch_size': 10},
+        {'traverse_paths': ['r', 'c'], 'batch_size': 5},
+    ],
+)
+def test_text_paddle(flow, content, document_array, parameters, mocker):
     def validate(resp):
         for doc in resp.docs:
             assert doc.embedding.shape == (1024,)
             assert doc.embedding.all()
 
+    mock_on_done = mocker.Mock()
     with flow as f:
-        f.index(inputs=document_array, on_done=validate)
+        f.index(inputs=document_array, on_done=mock_on_done)
+    validate_callback(mock_on_done, validate)
